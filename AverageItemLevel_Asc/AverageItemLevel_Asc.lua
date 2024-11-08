0e618e90c0ad48e1b0ef75b70d60bf39e068c1d7
Kyriazis Kokkinos <kyriaziskokkinos>
2024-11-08 18:56:55 +0200

    Added Icons to the tooltip Restructured the code

--- AverageItemLevel_Asc/AverageItemLevel_Asc.lua
+++ AverageItemLevel_Asc/AverageItemLevel_Asc.lua
@@ -1,166 +1,22 @@
-AverageItemLevel_Asc = select(2, ...)
-local _print = print
-local AIL = "|HAiLC|h" -- used as hidden text to be able to find our custom line in the tooltip easier
-local CACHE = {}
-local TIMEOUT = 60
-local debug = false
-local MAX_INSPECTIONS_TILL_TIMEOUT = 5
-local function print(...)
-	if debug then
-		_print(...)
-	end
-end
-
-function AverageItemLevel_Asc.toggleDebug()
-	debug = not debug
-	_print(debug and "Debug turned on." or "Debug turned off.")
-end
-
-local function getCacheForUnit(unit)
-	if not unit then
-		return
-	end
-	local guid = UnitGUID(unit)
-	if not CACHE[guid] then
-		CACHE[guid] = {}
-		CACHE[guid].spec = UnitClass(unit)
-		CACHE[guid].ilvl = 0
-		CACHE[guid].specExpirationTime = 0
-		CACHE[guid].ilvlExpirationTime = 0
-		CACHE[guid].inspections = 0
-	end
-	return CACHE[guid]
-end
-
-function AverageItemLevel_Asc.GetCache()
-	return CACHE
-end
-
-local function resetExpirations()
-	for _, data in pairs(CACHE) do
-		data.specExpirationTime = 0
-		data.ilvlExpirationTime = 0
-	end
-end
-
-function AverageItemLevel_Asc.SetInspectTimeout(newTimeout)
-	TIMEOUT = newTimeout > 0 and newTimeout or 1
-	resetExpirations()
-end
-
-local function IsIlvlThrottled(unit)
-	return getCacheForUnit(unit).ilvlExpirationTime > GetTime()
-end
-
-local function IsSpecThrottled(unit)
-	return getCacheForUnit(unit).specExpirationTime > GetTime()
-end
-
-local function notifyInspections(unit)
-	if AscensionInspectFrame and AscensionInspectFrame:IsShown() then
-		return
-	end
-	if CanInspect(unit) and not IsIlvlThrottled(unit) then
-		NotifyInspect(unit)
-	end
-	if C_MysticEnchant.CanInspect(unit) and not IsSpecThrottled(unit) then
-		C_MysticEnchant.Inspect(unit, true)
-	end
-end
-
-local function updateCacheSpec(unit)
-	if IsSpecThrottled(unit) then
-		return
-	end
-	local timeNow = GetTime()
-	local class, classFile = UnitClass(unit)
-	local neweSpec, _ = UnitSpecAndIcon(unit)
-	local data = getCacheForUnit(unit)
-	data.spec = neweSpec or data.spec or class or "?"
-
-	-- Is Hero --
-	if IsHeroClass(unit) then
-		-- if seasonal, spec == class so timeout instantly. if not, timeout when spec ~= class
-		if C_Realm.IsSeasonal() or data.spec ~= class then
-			data.specExpirationTime = timeNow + TIMEOUT
-		end
-	-- Is CoA --
-	elseif IsCustomClass(unit) then
-		-- Specialization inspections are not implemented yet by ascension
-		data.specExpirationTime = timeNow + TIMEOUT
-	end
-end
-
-local function updateCacheIlvl(unit)
-	if IsIlvlThrottled(unit) then
-		return
-	end
-	local ilvl = UnitAverageItemLevel(unit)
-	if ilvl == nil then
-		return
-	end
-	local data = getCacheForUnit(unit)
-	local timeNow = GetTime()
-	if ilvl > 0 and data.ilvl == ilvl then
-		print("Inspect result for", UnitName(unit), ":", data.ilvl, "-->", ilvl, ",saving.")
-		data.ilvlExpirationTime = timeNow + TIMEOUT
-		data.inspections = 0
-	elseif data.ilvl ~= ilvl or ilvl == 0 then
-		if data.inspections >= MAX_INSPECTIONS_TILL_TIMEOUT then
-			print("Reached inspection limit for", UnitName(unit), ",stopping.")
-			data.ilvlExpirationTime = timeNow + TIMEOUT
-			data.inspections = 0
-			return
-		end
-		print("Inspect result for", UnitName(unit), ":", data.ilvl, "-->", ilvl, ",repeating...")
-		data.ilvlExpirationTime = 0
-		data.ilvl = ilvl
-		-- scaling reporting wrong ilvl workaround
-		notifyInspections(unit)
-		data.inspections = data.inspections + 1
-	end
-end
-
-local function getColoredIlvlString(unit)
-	local itemLevel = getCacheForUnit(unit).ilvl
-	local color = WHITE_FONT_COLOR
-	local level = UnitLevel(unit)
-	local expansionTarget = Enum.Expansion.Vanilla
-	if level > 70 then
-		expansionTarget = Enum.Expansion.WoTLK
-	elseif level > 60 then
-		expansionTarget = Enum.Expansion.TBC
-	end
-	local softCap = GetItemLevelSoftCap(expansionTarget)
-	if itemLevel <= softCap then
-		color = ColorUtil:Lerp(color, ITEM_QUALITY_COLORS[Enum.ItemQuality.Epic], itemLevel / softCap)
-	else
-		-- if they have higher than the soft cap, give a special color
-		color = ITEM_QUALITY_COLORS[Enum.ItemQuality.Legendary]
-	end
-	if itemLevel > 0 then
-		local itemLevelText = format("%.02f", itemLevel)
-		return "(" .. color:WrapText(itemLevelText) .. ")"
-	else
-		color = ITEM_QUALITY_COLORS[Enum.ItemQuality.Poor]
-		return "(" .. color:WrapText("?") .. ")"
-	end
-end
+AiL = select(2, ...)
+AiL.Options = AiL.Options or {}
+AiL.hiddenText = "|HAiLC|h" -- used as hidden text to be able to find our custom line in the tooltip easier
 
 local function OnTooltipSetUnitHandler(self)
 	local _, unit = self:GetUnit()
 	if not unit or not UnitIsPlayer(unit) then
 		return
 	end
-	notifyInspections(unit)
-	local spec = getCacheForUnit(unit).spec
-	local color = ITEM_QUALITY_COLORS[Enum.ItemQuality.Legendary]
-	if spec == UnitClass(unit) or IsCustomClass(unit) then
-		color = RAID_CLASS_COLORS[select(2, UnitClass(unit))]
-	else
-		color = ITEM_QUALITY_COLORS[Enum.ItemQuality.Legendary]
-	end
-	self:AddDoubleLine(AIL .. color:WrapText(spec), getColoredIlvlString(unit))
+	local unitCache = AiL.getCacheForUnit(unit)
+	local spec = unitCache.spec
+	local icon = AiL.Options.ShowIcon and unitCache.icon or ""
+	local color = AiL.getColorforUnitSpec(unit, spec)
+	self:AddLine(" ")
+	self:AddDoubleLine(
+		AiL.hiddenText .. icon .. color:WrapText(spec),
+		AiL.getColoredIlvlString(UnitLevel(unit), AiL.getCacheForUnit(unit).true_ilvl)
+	)
+	AiL.notifyInspections(unit)
 end
 
 local function GameTooltipOnEvent(self, event, ...)
@@ -168,23 +24,45 @@ local function GameTooltipOnEvent(self, event, ...)
 	if not unit or not UnitIsPlayer(unit) then
 		return
 	end
+
+	-- if IsCustomClass(unit) then
+	-- 	AiL.updateCacheSpec(unit)
+	-- 	for i = 1, self:NumLines() do
+	-- 		if string.match(_G["GameTooltipTextLeft" .. i]:GetText() or "", AiL.hiddenText) then -- looks for our hidden text
+	-- 			local spec = AiL.getCacheForUnit(unit).spec
+	-- 			local color = AiL.getColorforUnitSpec(unit, spec)
+	-- 			_G["GameTooltipTextLeft" .. i]:SetText(AiL.hiddenText .. color:WrapText(spec))
+	-- 		end
+	-- 	end
+	-- end
+
 	if event == "INSPECT_TALENT_READY" then --UPDATE ILVL if > 0 and different than cached
-		updateCacheIlvl(unit)
 		for i = 1, self:NumLines() do
-			if string.match(_G["GameTooltipTextLeft" .. i]:GetText() or "", AIL) then -- looks for our hidden text
-				_G["GameTooltipTextRight" .. i]:SetText(getColoredIlvlString(unit))
+			if string.match(_G["GameTooltipTextLeft" .. i]:GetText() or "", AiL.hiddenText) then -- looks for our hidden text
+				_G["GameTooltipTextRight" .. i]:SetText(
+					AiL.getColoredIlvlString(UnitLevel(unit), AiL.getCacheForUnit(unit).true_ilvl)
+				)
 			end
 		end
-	elseif event == "MYSTIC_ENCHANT_INSPECT_RESULT" then -- UPDATE SPEC
-		updateCacheSpec(unit)
+		AiL.updateCacheIlvl(unit)
+	elseif event == "AIL_FINAL_INSPECT_REACHED" then
 		for i = 1, self:NumLines() do
-			if string.match(_G["GameTooltipTextLeft" .. i]:GetText() or "", AIL) then -- looks for our hidden text
-				local spec = getCacheForUnit(unit).spec
-				local color = ITEM_QUALITY_COLORS[Enum.ItemQuality.Legendary]
-				if spec == UnitClass(unit) or IsCustomClass(unit) then
-					color = RAID_CLASS_COLORS[select(2, UnitClass(unit))]
-				end
-				_G["GameTooltipTextLeft" .. i]:SetText(AIL .. color:WrapText(spec))
+			if string.match(_G["GameTooltipTextLeft" .. i]:GetText() or "", AiL.hiddenText) then -- looks for our hidden text
+				_G["GameTooltipTextRight" .. i]:SetText(
+					AiL.getColoredIlvlString(UnitLevel(unit), AiL.getCacheForUnit(unit).true_ilvl)
+				)
+			end
+		end
+	elseif event == "MYSTIC_ENCHANT_INSPECT_RESULT" and IsHeroClass(unit) then -- UPDATE CLASSLESS SPEC
+		AiL.updateCacheSpec(unit)
+		for i = 1, self:NumLines() do
+			if string.match(_G["GameTooltipTextLeft" .. i]:GetText() or "", AiL.hiddenText) then -- looks for our hidden text
+				local unitCache = AiL.getCacheForUnit(unit)
+				local spec = unitCache.spec
+				local icon = AiL.Options.ShowIcon and unitCache.icon or ""
+				local color = AiL.getColorforUnitSpec(unit, spec)
+				AiL.print("cached spec for ", unit, "is", spec)
+				_G["GameTooltipTextLeft" .. i]:SetText(AiL.hiddenText .. icon .. color:WrapText(spec))
 			end
 		end
 	end
@@ -193,6 +71,7 @@ end
 
 GameTooltip:RegisterEvent("INSPECT_TALENT_READY")
 GameTooltip:RegisterEvent("MYSTIC_ENCHANT_INSPECT_RESULT")
+GameTooltip:RegisterEvent("AIL_FINAL_INSPECT_REACHED")
 GameTooltip:HookScript("OnEvent", GameTooltipOnEvent)
 
 if GameTooltip:HasScript("OnTooltipSetUnit") then
---
+++ AverageItemLevel_Asc/core.lua
@@ -0,0 +1,187 @@
+AiL = select(2, ...)
+local CACHE = {}
+local TIMEOUT = 5
+local MAX_INSPECTIONS_TILL_TIMEOUT = 5
+local _print = print
+AiL.Options = AiL.Options or {}
+AiL.Options.ShowIcon = true
+AiL.Options.Debug = false
+--- DEBUG STUFF ---
+function AiL.print(...)
+	if AiL.Options.Debug then
+		_print(...)
+	end
+end
+
+function AiL.toggleDebug()
+	AiL.Options.Debug = not AiL.Options.Debug
+	_print(debug and "Debug turned on." or "Debug turned off.")
+end
+------ CACHE ------
+local function GetCache()
+	return CACHE
+end
+
+function AiL.getCacheForUnit(unit)
+	if not unit then
+		return
+	end
+	local guid = UnitGUID(unit)
+	if not CACHE[guid] then
+		local spec, icon = UnitSpecAndIcon(unit)
+
+		if IsCustomClass(unit) then
+			spec = (spec == UnitClass(unit)) and spec or (spec .. " " .. UnitClass(unit))
+			-- icon = "Interface\\Icons\\classicon_" .. UnitClass(unit):lower()
+		end
+		-- local spec,icon = UnitSpecAndIcon(unit)
+		-- spec = (spec == UnitClass(unit)) and spec
+		-- 		or (spec .. " " .. UnitClass(unit))
+
+		icon = " |T" .. icon .. ".blp:32:32:0:0|t "
+		CACHE[guid] = {
+			spec = spec,
+			icon = icon,
+			ilvl = 0,
+			true_ilvl = 0,
+			specExpirationTime = 0,
+			ilvlExpirationTime = 0,
+			inspections = 0,
+		}
+	end
+	return CACHE[guid]
+end
+
+function AiL.ClearCache()
+	CACHE = {}
+end
+
+function AiL.resetAllExpirations()
+	for _, data in pairs(CACHE) do
+		data.specExpirationTime = 0
+		data.ilvlExpirationTime = 0
+	end
+end
+
+function AiL.SetInspectTimeout(newTimeout)
+	TIMEOUT = newTimeout > 0 and newTimeout or 1
+	AiL.resetAllExpirations()
+end
+
+local function IsIlvlThrottled(unit)
+	return AiL.getCacheForUnit(unit).ilvlExpirationTime > GetTime()
+end
+
+local function IsSpecThrottled(unit)
+	return AiL.getCacheForUnit(unit).specExpirationTime > GetTime()
+end
+
+function AiL.updateCacheSpec(unit)
+	if IsSpecThrottled(unit) then
+		return
+	end
+	local timeNow = GetTime()
+	local class, classFile = UnitClass(unit)
+	local newSpec, newIcon = UnitSpecAndIcon(unit)
+	newIcon = " |T" .. newIcon .. ".blp:32:32:0:0|t "
+	local data = AiL.getCacheForUnit(unit)
+	-- Is Hero --
+	if IsHeroClass(unit) then
+		-- if seasonal, spec == class so timeout instantly. if not, timeout when spec ~= class
+		data.spec = newSpec or data.spec or class or "?"
+		data.icon = newIcon
+		if C_Realm.IsSeasonal() or data.spec ~= class then
+			data.specExpirationTime = timeNow + TIMEOUT
+		end
+
+	-- Is CoA --
+	elseif IsCustomClass(unit) then
+		data.spec = newSpec
+
+		if newSpec ~= UnitClass(unit) then -- UnitSpecAndIcon returned Specialization so we need to append the class
+			data.spec = newSpec .. " " .. UnitClass(unit)
+		end
+		-- Specialization inspections are not implemented yet by ascension
+		data.specExpirationTime = timeNow + TIMEOUT
+	end
+end
+
+function AiL.notifyInspections(unit)
+	if AscensionInspectFrame and AscensionInspectFrame:IsShown() then
+		return
+	end
+
+	if CanInspect(unit) and not IsIlvlThrottled(unit) then
+		NotifyInspect(unit)
+	end
+	if IsHeroClass(unit) then
+		if C_MysticEnchant.CanInspect(unit) and not IsSpecThrottled(unit) then
+			C_MysticEnchant.Inspect(unit, true)
+		end
+	end
+end
+
+function AiL.updateCacheIlvl(unit)
+	if IsIlvlThrottled(unit) then
+		return
+	end
+	local ilvl = UnitAverageItemLevel(unit)
+	if ilvl == nil then
+		return
+	end
+	local data = AiL.getCacheForUnit(unit)
+	local timeNow = GetTime()
+	if ilvl > 0 and data.ilvl == ilvl then
+		AiL.print("Inspect result for", UnitName(unit), ":", data.ilvl, "-->", ilvl, ",saving.")
+		data.ilvlExpirationTime = timeNow + TIMEOUT
+		data.inspections = 0
+		data.true_ilvl = ilvl
+		C_Hook:SendBlizzardEvent("AIL_FINAL_INSPECT_REACHED")
+	elseif data.ilvl ~= ilvl or ilvl == 0 then
+		if data.inspections >= MAX_INSPECTIONS_TILL_TIMEOUT then
+			AiL.print("Reached inspection limit for", UnitName(unit), ",stopping.")
+			data.ilvlExpirationTime = timeNow + TIMEOUT
+			data.inspections = 0
+			return
+		end
+		AiL.print("Inspect result for", UnitName(unit), ":", data.ilvl, "-->", ilvl, ",repeating...")
+		data.ilvlExpirationTime = 0
+		data.ilvl = ilvl
+		-- scaling reporting wrong ilvl workaround
+		AiL.notifyInspections(unit)
+		data.inspections = data.inspections + 1
+	end
+end
+
+function AiL.getColorforUnitSpec(unit, spec)
+	local color = ITEM_QUALITY_COLORS[Enum.ItemQuality.Legendary]
+	if spec == UnitClass(unit) or IsCustomClass(unit) then
+		color = RAID_CLASS_COLORS[select(2, UnitClass(unit))]
+	end
+	return color
+end
+
+function AiL.getColoredIlvlString(unitLevel, itemLevel)
+	local color = WHITE_FONT_COLOR
+	local expansionTarget = Enum.Expansion.Vanilla
+
+	if unitLevel > 70 then
+		expansionTarget = Enum.Expansion.WoTLK
+	elseif unitLevel > 60 then
+		expansionTarget = Enum.Expansion.TBC
+	end
+
+	local softCap = GetItemLevelSoftCap(expansionTarget)
+	if itemLevel <= softCap then
+		color = ColorUtil:Lerp(color, ITEM_QUALITY_COLORS[Enum.ItemQuality.Epic], itemLevel / softCap)
+	else
+		color = ITEM_QUALITY_COLORS[Enum.ItemQuality.Legendary]
+	end
+
+	if itemLevel > 0 then
+		return "(" .. color:WrapText(format("%.02f", itemLevel)) .. ")"
+	else
+		color = ITEM_QUALITY_COLORS[Enum.ItemQuality.Poor]
+		return "(" .. color:WrapText("?") .. ")"
+	end
+end
